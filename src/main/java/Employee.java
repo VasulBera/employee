09bd34b091d2e81fc3a87cd64c04e1b4c626410b
Vasyl Bervetskyi <vberv@softserveinc.com>
2019-03-06 19:27:47 +0200

    five

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -7,7 +7,7 @@ public class Employee {
     private String lastName;
     private Employee manager;
     private List<Employee> subordinates;
-	rty
+	rty sdfsdfdsf
 
     public Employee(String firstName, String lastName) {
         this.firstName = firstName;
