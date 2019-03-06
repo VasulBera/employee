bca00e41f13022afbfb4b6654c8b4611df118f43
Vasyl Bervetskyi <vberv@softserveinc.com>
2019-03-06 18:49:27 +0200

    remove one

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -8,7 +8,6 @@ public class Employee {
     private Employee manager;
     private List<Employee> subordinates;
     private String one;
-	private String two;
 
     public Employee(String firstName, String lastName) {
         this.firstName = firstName;
